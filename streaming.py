522a4b132a439c4fabab9bc281c03e30e8f9218d
kapsalis <kapsalis1989@gmail.com>
2016-09-21 17:45:45 +0200

    Streaming Twitter
    Streaming the twitter with keywords.
    Getting info from the tweets for: tweet_time, tweet_id, user_id, number of friends and followers of the user and the text of tweet.
    
    (Retrieving the messages in English Language. (not yet))

--- streaming.py
+++ streaming.py
@@ -2,32 +2,34 @@ import tweepy
 from tweepy import OAuthHandler
 from tweepy import StreamListener
 from tweepy import Stream
+import configparser
 import datetime
-import config
 
 class StdOutListener(StreamListener):
     def on_data(self,data):
-        tweet_lang = data.split(',"lang":"')[1].split('","timestamp_ms')[0]
-        if tweet_lang.lower() != "en":
-            tweet_id = data.split(',"id_str":"')[1].split('","text')[0]
-            tweet_text = data.split(',"text":"')[1].split('","source')[0]
-            tweet_timestamp = data.split(',"timestamp_ms":"')[1].split('"')[0]
-            
-            user_info = data.split(',"user":{')[1].split('","geo')[0]
-            user_id = user_info.split('"id":')[1].split(',"id_str')[0]
-            user_nr_followers =  user_info.split(',"followers_count":')[1].split(',"friends_count')[0]
-            user_nr_friends = user_info.split(',"friends_count":')[1].split(',"listed_count')[0]
-            
-            tweet_time = self.convert_timestamp(tweet_timestamp)
-            
-            try:
-                file = open('dataset/tweets_drugs_alcohol.csv','a')
-                file.write(str(tweet_time) + ',' + tweet_id + ',' + user_id + ',' + user_nr_followers + ',' + user_nr_friends + ',' + tweet_text)		
-                file.write('\n')
-                file.close()
-                return True
-            except IOError:
-                print("Could not read the file")
+        #print(data)
+        #tweet_lang = data.split(',"lang":"')[1].split('","timestamp_ms')[0]
+        #print(tweet_lang)
+        #if tweet_lang.lower() != "en":
+        tweet_id = data.split(',"id_str":"')[1].split('","text')[0]
+        tweet_text = data.split(',"text":"')[1].split('","source')[0]
+        tweet_timestamp = data.split(',"timestamp_ms":"')[1].split('"')[0]
+        
+        user_info = data.split(',"user":{')[1].split('","geo')[0]
+        user_id = user_info.split('"id":')[1].split(',"id_str')[0]
+        user_nr_followers =  user_info.split(',"followers_count":')[1].split(',"friends_count')[0]
+        user_nr_friends = user_info.split(',"friends_count":')[1].split(',"listed_count')[0]
+        
+        tweet_time = self.convert_timestamp(tweet_timestamp)
+        
+        try:
+            file = open('dataset/tweets_drugs_alcohol.csv','a')
+            file.write(str(tweet_time) + ',' + tweet_id + ',' + user_id + ',' + user_nr_followers + ',' + user_nr_friends + ',' + tweet_text)		
+            file.write('\n')
+            file.close()
+            return True
+        except IOError:
+            print("Could not read the file")
             
 
     def convert_timestamp(self, timestamp):
@@ -46,8 +48,17 @@ if __name__ == '__main__':
     file.close()
     
     l = StdOutListener()
-    auth = tweepy.OAuthHandler(config.consumer_key,config.consumer_secret)
-    auth.set_access_token(config.access_token, config.access_secret)
+    
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    
+    consumer_key = config.get('Twitter', 'consumer_key')
+    consumer_secret = config.get('Twitter', 'consumer_secret')
+    access_token = config.get('Twitter', 'access_token')
+    access_secret = config.get('Twitter', 'access_secret')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
 
     stream = Stream(auth,l)
 
